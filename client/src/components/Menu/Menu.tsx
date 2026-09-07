b7f54884d93adb9d74eb018c48aa40723eefc1fa
yigitlevent <yigitlevent@live.com>
2026-09-07 16:19:05 +0300

    minor unused import fix

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Group, Paper, Tooltip } from "@mantine/core";
+import { ActionIcon, Group, Tooltip } from "@mantine/core";
 import { BookOpen, ClipboardCheck, ClipboardList, Database, LogIn, LogOut, BookOpenCheck } from "lucide-react";
 import { Fragment, useState } from "react";
 
