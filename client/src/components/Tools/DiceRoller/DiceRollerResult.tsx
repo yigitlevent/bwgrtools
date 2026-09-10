877c3e94898d9bc3535f7792ed50e9452eda5670
yigitlevent <yigitlevent@live.com>
2026-09-10 16:42:57 +0300

    0.2.56 refactors dice roller

--- client/src/components/Tools/DiceRoller/DiceRollerResult.tsx
+++ client/src/components/Tools/DiceRoller/DiceRollerResult.tsx
@@ -1,5 +1,5 @@
-import { Button, Divider, Grid, Text, Title, Tooltip } from "@mantine/core";
-import { Dice1, Dice2, Dice3, Dice4, Dice5, Dice6 } from "lucide-react";
+import { ActionIcon, Divider, Grid, Text, Title, Tooltip, Group } from "@mantine/core";
+import { Dice1, Dice2, Dice3, Dice4, Dice5, Dice6, Dices } from "lucide-react";
 import { Fragment } from "react";
 
 import type { TestResult } from "./DiceRoller";
@@ -29,47 +29,101 @@ export function DiceRollerResult({ result, shade, isDoubleObstacle, isOpenEnded,
             <Divider label="Result" mt="10px" />
 
             <Grid columns={3}>
-              <Grid.Col span={{ base: 3, sm: 1 }}>
-                <Title order={6}>Result</Title>
+              <Grid.Col span={{ base: 2, sm: 1 }}>
+                <Title order={5}>Result</Title>
                 <Text>{getResultText(result)}</Text>
               </Grid.Col>
 
-              <Grid.Col span={{ base: 3, sm: 1 }}>
-                <Fragment>
-                  <Title order={6}>Dice</Title>
-
-                  <Text>
-                    {result.dice.map((v, i) => {
-                      if (v === 1) return <Dice1 key={i} color="var(--mantine-color-red-6)" style={{ marginBottom: -6 }} />;
-                      else if (v === 2) return <Dice2 key={i} color={(shade === "White") ? "var(--mantine-color-green-6)" : "var(--mantine-color-red-6)"} style={{ marginBottom: -6 }} />;
-                      else if (v === 3) return <Dice3 key={i} color={(shade === "Gray") ? "var(--mantine-color-green-6)" : "var(--mantine-color-red-6)"} style={{ marginBottom: -6 }} />;
-                      else if (v === 4) return <Dice4 key={i} color="var(--mantine-color-green-6)" style={{ marginBottom: -6 }} />;
-                      else if (v === 5) return <Dice5 key={i} color="var(--mantine-color-green-6)" style={{ marginBottom: -6 }} />;
-                      else return <Dice6 key={i} color="var(--mantine-color-green-6)" style={{ marginBottom: -6 }} />;
-                    })}
-                  </Text>
-                </Fragment>
-
-                {!isOpenEnded && result.dice.includes(6) && !result.usedFate
-                  ? (
-                    <Tooltip multiline w={320} color="gray" label="Spend a Fate point to reroll all 6s in this pool, even though Open Ended is off.">
-                      <Button variant="outline" size="md" onClick={() => { rerollSixes(result.dice, true); }} mt="24px">Reroll sixes using Fate</Button>
-                    </Tooltip>
-                  )
-                  : null}
-
-                {isOpenEnded && result.failures > 0 && !result.usedFate
-                  ? (
-                    <Tooltip multiline w={320} color="gray" label="Spend a Fate point to reroll one failed die from this pool.">
-                      <Button variant="outline" size="md" onClick={() => { rerollFailure(result.dice); }} mt="6px">Reroll a single failure using Fate</Button>
-                    </Tooltip>
-                  )
-                  : null}
+              <Grid.Col span={{ base: 1, sm: 1 }}>
+                <Title order={5}>Test</Title>
+                <Text>{result.test}</Text>
               </Grid.Col>
 
               <Grid.Col span={{ base: 3, sm: 1 }}>
-                <Title order={6}>Test</Title>
-                <Text>{result.test}</Text>
+                <Title order={5}>Dice</Title>
+
+                <Group justify="start" align="center" gap={2}>
+                  {result.dice.map((v, i) => {
+                    if (v === 1) {
+                      return (
+                        <Dice1
+                          key={i}
+                          size="30px"
+                          color="var(--mantine-color-red-6)"
+                        />
+                      );
+                    }
+                    else if (v === 2) {
+                      return (
+                        <Dice2
+                          key={i}
+                          size="30px"
+                          color={(shade === "White") ? "var(--mantine-color-green-6)" : "var(--mantine-color-red-6)"}
+                        />
+                      );
+                    }
+                    else if (v === 3) {
+                      return (
+                        <Dice3
+                          key={i}
+                          size="30px"
+                          color={(shade === "Gray") ? "var(--mantine-color-green-6)" : "var(--mantine-color-red-6)"}
+                        />
+                      );
+                    }
+                    else if (v === 4) {
+                      return (
+                        <Dice4
+                          key={i}
+                          size="30px"
+                          color="var(--mantine-color-green-6)"
+                        />
+                      );
+                    }
+                    else if (v === 5) {
+                      return (
+                        <Dice5
+                          key={i}
+                          size="30px"
+                          color="var(--mantine-color-green-6)"
+                        />
+                      );
+                    }
+                    else {
+                      return (
+                        <Dice6
+                          key={i}
+                          size="30px"
+                          color="var(--mantine-color-green-6)"
+                        />
+                      );
+                    }
+                  })}
+
+                  {result.dice.includes(6) && !result.usedFate
+                    ? (
+                      <Tooltip
+                        multiline
+                        w={320}
+                        color="gray"
+                        label={
+                          isOpenEnded
+                            ? "Spend a Fate point to reroll one failed die from this pool."
+                            : "Spend a Fate point to reroll all 6s in this pool, even though Open Ended is off."
+                        }
+                      >
+                        <ActionIcon size="30px" p={1} ml={8} variant="light">
+                          <Dices
+                            onClick={() => {
+                              if (isOpenEnded) rerollFailure(result.dice);
+                              else rerollSixes(result.dice, true);
+                            }}
+                          />
+                        </ActionIcon>
+                      </Tooltip>
+                    )
+                    : null}
+                </Group>
               </Grid.Col>
             </Grid>
           </Fragment>
